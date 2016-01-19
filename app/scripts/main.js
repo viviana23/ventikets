338bde1caa9b05e72be25cb5e9f039fc9cf36946
viviana23 <rinconviviana22@gmail.com>
2016-01-19 18:35:34 -0430

    segundo commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,5 @@
 $.getJSON("eventos.json", function(data) {
     console.log(data);
-
     var today = $("#hoyTemplate").html();
     var notavailable = $("#agotadoTemplate").html();
     var defaulttemplate = $("#eventTemplate").html();
@@ -8,21 +7,72 @@ $.getJSON("eventos.json", function(data) {
         console.log(val.today)
         if (val.today) {
             var template = Handlebars.compile(today);
+
         } else if (val.available) {
             var template = Handlebars.compile(notavailable);
         } else {
             var template = Handlebars.compile(defaulttemplate);
+
         }
         var html = template(val);
         $("#eventos").append(html);
 
+
+
     });
-    
+
+    //lugar-fecha-hora del evento en movil//
+    var onlymovil = $("#moviltemplate").html();
+    $.each(data.eventos, function(key, val) {
+        console.log(val.today)
+        var template = Handlebars.compile(onlymovil);
+        var html = template(val);
+        $("#eventosmovil").append(html);
+
+    });
+
+});
+//puntos de venta-cotacto-correo//
+$.getJSON("eventos.json", function(data) {
+    console.log(data);
+    var source = $("#puntostemplate").html();
+    var template = Handlebars.compile(source);
+    $.each(data.puntos, function(key, val) {
+        var html = template(val);
+        console.log(html);
+        $("#puntos").append(html);
+
+    });
+    var source = $("#numerostemplate").html();
+    var template = Handlebars.compile(source);
+    $.each(data.numeros, function(key, val) {
+        var html = template(val);
+        console.log(html);
+        $("#numeros").append(html);
+
+    });
+    var source = $("#correotemplate").html();
+    var template = Handlebars.compile(source);
+    $.each(data.correo, function(key, val) {
+        var html = template(val);
+        console.log(html);
+        $("#correo").append(html);
+
+    });
+
 });
 
+$(document).on("click","#only",function(evt){
+   $.getJSON("eventos.json", function(data) {
+ 
+    $.each(data, function(key, val) {
+        console.log(html);
+  $("#detalleTemplate #titulo").val( $(this).data( "nombre" ) );
+});
+});
+});
 
-
-
+//mostrar u ocultar paneles en evento.html//
 
 $("#continuar").click(function() {
     $("#asientos").hide(0, function() {
